a43d2f582ba5d6f9147c32d7be1ab4ebbbb3fd75
Quinn Ramsay <qramsay7@gmail.com>
2021-06-24 19:26:49 -0700

    Updating heroku and local connections

--- frontend/src/services/game.js
+++ frontend/src/services/game.js
@@ -1,9 +1,13 @@
 import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
 // open connection
-var connection = new WebSocket('ws://127.0.0.1:3001');
+
+let websocketURL = process.env.NODE_ENV === 'development' ? 'ws://127.0.0.1:3001' : 'wss://' + window.location.host
+var connection = new WebSocket(websocketURL)
+
+
 connection.onopen = function () {
-};
+}
 
 let handlers = []
 let allowedMovements = []
@@ -11,7 +15,7 @@ let allowedMovements = []
 connection.onerror = function (error) {
     console.log('Sorry, but there\'s some problem with your '
         + 'connection or the server is down.')
-};
+}
 
 // most important part - incoming messages
 connection.onmessage = function (message) {
@@ -25,7 +29,7 @@ connection.onmessage = function (message) {
     for (let handler of handlers) {
         handler.handle(json)
     }
-};
+}
 
 
 setInterval(function () {
