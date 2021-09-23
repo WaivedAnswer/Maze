8c2641981e362539a5d9fd195579283950c46da3
Quinn Ramsay <qramsay7@gmail.com>
2021-09-22 20:45:57 -0700

    removing section creation from section, add tests

--- backend/tests/section.test.js
+++ backend/tests/section.test.js
@@ -1,8 +1,63 @@
 const Section = require('../models/section').Section
 const Coordinate = require('../models/coordinate').Coordinate
 
-test('section  y', () => {
-    const section = new Section(4, new Coordinate(0, 0), null)
-    const result = section.canMove(new Coordinate(0, 0))
-    expect(result).toBe(true)
+test('section relativeTo origin', () => {
+    const origin = new Coordinate(0, 0)
+    const section = new Section(4, origin)
+    const testCoordinate = new Coordinate(5,5)
+
+    const result = section.getRelativeCoord(testCoordinate)
+
+    expect(result).toStrictEqual(testCoordinate)
+})
+
+test('section relativeTo positive', () => {
+    const offset = new Coordinate(3, 2)
+    const section = new Section(4, offset)
+    const testCoordinate = new Coordinate(5, 5)
+
+    const result = section.getRelativeCoord(testCoordinate)
+
+    expect(result).toStrictEqual(new Coordinate(2, 3))
+})
+
+test('section relativeTo negative', () => {
+    const offset = new Coordinate(-3, -4)
+    const section = new Section(4, offset)
+    const testCoordinate = new Coordinate(5, 5)
+
+    const result = section.getRelativeCoord(testCoordinate)
+
+    expect(result).toStrictEqual(new Coordinate(8, 9))
+})
+
+test('section no walls', () => {
+    const offset = new Coordinate(0, 0)
+    const section = new Section(4, offset)
+
+    const result = section.getWalls()
+
+    expect(result).toStrictEqual([])
+})
+
+test('section single wall', () => {
+    const offset = new Coordinate(0, 0)
+    const section = new Section(4, offset)
+    section.addWall(new Coordinate(0, 0))
+
+    const result = section.getWalls(new Coordinate(0,0))
+
+    expect(result).toEqual(expect.arrayContaining([{ x:0, y:0 }]))
+})
+
+test('section multi wall', () => {
+    const offset = new Coordinate(0, 0)
+    const section = new Section(4, offset)
+    section.addWall(new Coordinate(0,0))
+    section.addWall(new Coordinate(1,1))
+    section.addWall(new Coordinate(2,2))
+
+    const result = section.getWalls(new Coordinate(0,0))
+
+    expect(result).toEqual(expect.arrayContaining([{ x:0, y:0 }, { x:1, y:1 }, { x:2, y:2 }]))
 })
