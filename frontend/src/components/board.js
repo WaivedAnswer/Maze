e7bd49d0cefb1dfd6a5753efaa17984906af5137
Quinn Ramsay <qramsay7@gmail.com>
2023-07-29 16:17:56 -0700

    Select your portal

--- frontend/src/components/board.js
+++ frontend/src/components/board.js
@@ -1,15 +1,33 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 
 
 import BoardContext from './boardContext'
 import Tile from './tile'
 import {TileType} from '../models/tile'
+
 import Token  from './token'
 import Escalator from './escalator'
 import Wall from './wall'
 
 const Board = ({ gameState, board, tokens, escalators, walls, gameService }) => {
-    const myRef = useRef(null)
+    const boardRef = useRef(null)
+
+    const gridHeight = board.getHeight()
+    const gridWidth = board.getWidth()
+    const scrollToTile = (tile) => {
+        if (boardRef.current) {
+          const boardRect = boardRef.current.getBoundingClientRect();
+    
+          const scrollX = elementRect.left + elementRect.width / 2 - ancestorRect.left - ancestorRect.width / 2;
+          const scrollY = elementRect.top + elementRect.height / 2 - ancestorRect.top - ancestorRect.height / 2;
+    
+          ancestorRef.current.scrollTo({
+            top: ancestorRef.current.scrollTop + scrollY,
+            left: ancestorRef.current.scrollLeft + scrollX,
+            behavior: 'smooth',
+          });
+        }
+    
     const handleKeyPress = (event) => {
         if (event.key.toLowerCase() === 's') {
             gameService.moveDown()
@@ -24,16 +42,21 @@ const Board = ({ gameState, board, tokens, escalators, walls, gameService }) => 
             if(selectedToken) {
                 const currTile = board.getTile(selectedToken.coord)
                 if(currTile.type === TileType.PORTAL) {
-                    //
-                    //portalSelector.handle()
-                    /* if(state === selection) {}
-                    */
+                    board.portalSelector.handle(selectedToken, currTile)
                 } else {
                     gameService.escalate()
                 }
             }
+        } else if (event.key === 'Enter') {
+            if(!board.portalSelector.isActive()) {
+                return
+            }
+            //TODO change to id based 
+            const selectedPortal = board.portalSelector.select()
+            gameService.teleport(selectedPortal.coord)
         }
     }
+
 
     const onTokenSelected = (token) => {
         gameService.send(JSON.stringify(
@@ -52,10 +75,6 @@ const Board = ({ gameState, board, tokens, escalators, walls, gameService }) => 
         return board.getDirection(tile)
     }
 
-    const gridHeight = board.getHeight()
-
-    const gridWidth = board.getWidth()
-
     const boardStyle = {
         padding: '36px',
         overflow: 'auto',
@@ -66,8 +85,8 @@ const Board = ({ gameState, board, tokens, escalators, walls, gameService }) => 
      } 
 
     return (
-        <BoardContext.Provider value={myRef}>
-            <div className="game-board" ref={myRef} style= {boardStyle} onKeyDown={handleKeyPress} tabIndex={0}>
+        <BoardContext.Provider value={boardRef}>
+            <div className="game-board" ref={boardRef} style= {boardStyle} onKeyDown={handleKeyPress} tabIndex={0}>
                 {
                     board.grid.map((row, rowNum) =>
                             row.map((tile, colNum) =>
