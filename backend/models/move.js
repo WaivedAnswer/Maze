66dad33d77aaa71fece87a6b4ef793c528160ff6
Quinn Ramsay <qramsay7@gmail.com>
2023-08-15 08:07:35 -0700

    moves more evenly distributed

--- backend/models/move.js
+++ backend/models/move.js
@@ -5,15 +5,17 @@ const splitMoves = (movements, playerCount) => {
 
     const playerMoves = new Array(playerCount).fill(0).map(() => [])
 
+    const randomStartingPlayerIdx = Math.floor(Math.random() * playerCount)
+    let playerIdx = randomStartingPlayerIdx
     while (movements.length > 0) {
-        const randomPlayerIdx = Math.floor(Math.random() * playerCount)
         const randomMoveIdx = Math.floor(Math.random() * movements.length)
 
-        // Assign the randomly selected move to the randomly selected player
-        playerMoves[randomPlayerIdx].push(movements[randomMoveIdx])
-
+        // Assign the randomly selected move to the next Player
+        playerMoves[playerIdx].push(movements[randomMoveIdx])
         // Remove the assigned move from the movements array
         movements.splice(randomMoveIdx, 1)
+
+        playerIdx = (playerIdx + 1) % playerCount
     }
 
     return playerMoves
